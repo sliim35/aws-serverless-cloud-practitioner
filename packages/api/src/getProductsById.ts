ff339c1862f08572a74abb5e5fdccf454ff7e90c
Maksim Dvoinishnikov <maksim_dvoinishnikov@epam.com>
2023-02-27 17:35:43 +0400

    Rename packages

--- packages/api/src/getProductsById.ts
+++ packages/api/src/getProductsById.ts
@@ -4,16 +4,6 @@ import { products } from '../mocks/products';
 // Type
 import type { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
     const { pathParameters } = event;
 
